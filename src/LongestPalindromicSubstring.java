aac3bc3679230faa59c37ef7bd3daa2df37c1a3a
ghost2004 <liutg@hotmail.com>
2015-02-08 22:10:30 -0800

    comments for palindromic algorithm

--- src/LongestPalindromicSubstring.java
+++ src/LongestPalindromicSubstring.java
@@ -9,14 +9,18 @@ public class LongestPalindromicSubstring {
         if (s == null || s.length() < 2) 
             return s;
         int len = s.length();
+        // Matrix for palindrome, p[i][j] is true when substring(i,j) is palindrome
         boolean p[][] = new boolean [len][len];
-        
+        // variables for longest substring
         int max_start = 0;
         int max_end = 1;
         int max_len = 0;
         
         for (int i = len-1; i >=0; i--) {
             for (int j = i; j < len; j++) {
+                // Only 2 conditions will make p[i][j] true
+                // 1. s[i] == s[j] && p[i+1][j-1]  like b (true) --> aba ( true)
+                // 2. s[i] == s[j] && j = i +1  like bb(true)
                 if (s.charAt(i) == s.charAt(j) && 
                         ((j-i <2)||(p[i+1][j-1]))){
                     p[i][j] = true;
