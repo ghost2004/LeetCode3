ac28255d0e1c7897ec0539176fc6ef7abc87b5c9
ghost2004 <liutg@hotmail.com>
2015-02-25 22:39:23 -0800

    Comments

--- src/ClimbingStairs.java
+++ src/ClimbingStairs.java
@@ -5,11 +5,15 @@ Each time you can either climb 1 or 2 steps. In how many distinct ways
  can you climb to the top?
  */
 public class ClimbingStairs {
+    // Typical Fibonacci numbers 
     public int climbStairs(int n) {
+        // For 1 stair, 1 way
+        // For 2 stairs, 2 ways 1+1
         if ( n < 2)
             return n;
         int a = 1;
         int b = 2;
+        // S[n] = S[n-2] + S[n-1]
         for (int i = 3; i <= n;i++) {
             int c = b;
             b += a;
