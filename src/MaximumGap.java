01259a69380e234e32dff4b9ff7f3f96404ad810
ghost2004 <tliu@tliu-mac.local>
2015-05-01 17:12:57 -0700

    Comments

--- src/MaximumGap.java
+++ src/MaximumGap.java
@@ -21,27 +21,34 @@ public class MaximumGap {
         int max = num[0];
         int i;
         int idx;
+        // find the max and min value in the array
         for (i = 1; i< num.length; i++) {
             min = Math.min(min, num[i]);
             max = Math.max(max, num[i]);
         }
-        
+        // the minimal gap will be (max-min)/len +1
         int gap = (max - min)/num.length + 1;
+        // the number of buckets
         int len = (max - min)/gap +1;
         
+        // the minimal value of the bucket
         int bucketMin[] = new int[len]; 
+        // the maximal value of the bucket
         int bucketMax[] = new int[len];
+        // the flag that if the bucket is filled
         boolean bucketFilled[] = new boolean[len];
         
         Arrays.fill(bucketFilled, false);
         
         for (i = 0; i < num.length; i++) {
-
+            // get the bucket number
             idx = (num[i] - min) / gap;
             if (bucketFilled[idx]) {
+                // if numbers in this bucket, change the max/min numbers
                 bucketMin[idx] = Math.min(bucketMin[idx] , num[i]);
                 bucketMax[idx] = Math.max(bucketMax[idx] , num[i]);
             } else {
+                // the first element in this bucket
                 bucketFilled[idx] = true;
                 bucketMin[idx] = num[i];
                 bucketMax[idx] = num[i];
@@ -49,6 +56,7 @@ public class MaximumGap {
         }
         int prev = 0;
         
+        // scan the buckets
         for (idx = 1; idx < len;idx++) {
             if (!bucketFilled[idx])
                 continue;
