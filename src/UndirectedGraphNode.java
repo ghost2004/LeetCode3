e67ccd5af264a7f92ee993e368163b6a8cb18653
ghost2004 <liutg@hotmail.com>
2015-07-30 23:24:13 -0700

    Clone graph

---
+++ src/UndirectedGraphNode.java
@@ -0,0 +1,6 @@
+import java.util.*;
+public class UndirectedGraphNode {
+    int label;
+    List<UndirectedGraphNode> neighbors;
+    UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+}
