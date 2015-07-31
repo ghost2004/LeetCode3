ea66c8b5244f3706cf630c2b356fc5773b402bb3
ghost2004 <liutg@hotmail.com>
2015-07-30 23:29:00 -0700

    Power of 2

---
+++ src/PowerofTwo.java
@@ -0,0 +1,12 @@
+/*
+ * Given an integer, write a function to determine if it is a power of two.
+ */
+public class PowerofTwo {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0)
+            return false;
+        int x = n;
+        
+        return true;
+    }
+}
