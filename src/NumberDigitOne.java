4d31b5a8a8c1beb0bb0dc878db8e8d37989a6a89
ghost2004 <liutg@hotmail.com>
2015-08-02 21:18:33 -0700

    comments

--- src/NumberDigitOne.java
+++ src/NumberDigitOne.java
@@ -5,6 +5,38 @@ For example:
 Given n = 13,
 Return 6, because digit 1 occurred in the following numbers: 1, 10, 11, 12, 13.
  */
+
+/*
+ * 
+one's number        numbers with one                                                                     Section
+1                   1                                                                                     [1, 9]
+
+11                  10  11  12  13  14  15  16  17  18  19                                                [10, 19]
+
+1                   21                                                                                   [20, 29]
+
+1                   31                                                                                   [30, 39]
+
+1                   41                                                                                   [40, 49]
+
+1                   51                                                                                   [50, 59]
+
+1                   61                                                                                   [60, 69]
+
+1                   71                                                                                   [70, 79]
+
+1                   81                                                                                   [80, 89]
+
+1                   91                                                                                   [90, 99]
+
+11                 100  101  102  103  104  105  106  107  108  109                                      [100, 109]
+
+21                 110  111  112  113  114  115  116  117  118  119                                      [110, 119]
+
+11                 120  121  122  123  124  125  126  127  128  129                                      [120, 129]
+
+...                  ...                                                                                  ...
+ */
 public class NumberDigitOne {
     public int countDigitOne(int n) {
 
