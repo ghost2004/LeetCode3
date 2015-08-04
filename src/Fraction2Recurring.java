e3253f6480335b0dd1b7de7fa61c1364dd737520
ghost2004 <tliu@tliu-mac.local>
2015-08-03 18:42:27 -0700

    Fraction to Recurring Decimal draft

---
+++ src/Fraction2Recurring.java
@@ -0,0 +1,21 @@
+/*
+ * Given two integers representing the numerator and denominator of a fraction, return the fraction in string format.
+
+If the fractional part is repeating, enclose the repeating part in parentheses.
+
+For example,
+
+Given numerator = 1, denominator = 2, return "0.5".
+Given numerator = 2, denominator = 1, return "2".
+Given numerator = 2, denominator = 3, return "0.(6)".
+
+ */
+import java.util.*;
+public class Fraction2Recurring {
+    public String fractionToDecimal(int numerator, int denominator) {
+        StringBuffer t = new StringBuffer();
+        
+        
+        return t.toString();
+    }
+}
