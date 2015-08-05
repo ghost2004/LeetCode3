fa0292daae0aa8f129427a38986c31da3bf3bc70
ghost2004 <liutg@hotmail.com>
2015-08-04 23:02:06 -0700

    Fraction to Recurring Decimal

--- src/Fraction2Recurring.java
+++ src/Fraction2Recurring.java
@@ -11,11 +11,68 @@ Given numerator = 2, denominator = 3, return "0.(6)".
 
  */
 import java.util.*;
+
 public class Fraction2Recurring {
     public String fractionToDecimal(int numerator, int denominator) {
-        StringBuffer t = new StringBuffer();
+        if (numerator == 0)
+            return "0";
+        if (denominator == 0)
+            return "";
+        String result = "";
         
+        // get the sign first
+        if ((numerator < 0) ^ (denominator < 0)) {
+            result += "-";
+        }
         
-        return t.toString();
+        // turn the number into unsigned number
+        // use long to avoid overflow
+        long num = numerator, den = denominator;
+        num = Math.abs(num);
+        den = Math.abs(den);
+        
+        // quotient 
+        long res = num/den;
+        result += String.valueOf(res);
+        
+        // get the reminder first, if it is zero then we are done
+        long reminder = (num % den) * 10;
+        if (reminder == 0)
+            return result;
+        result += ".";
+        // map for the reminder and index in the string
+        HashMap<Long, Integer> map = new HashMap<Long, Integer>();
+        
+        // keep running until reminder is zero
+        while (reminder != 0 ){
+            // check the reminder in the map
+            Integer idx = map.get(reminder);
+            if (idx != null) {
+                // the reminder is showed up before, so here is the infinity loop
+                // get the left part of infinity loop
+                String left = result.substring(0, idx);
+                // get the right part of infinity loop
+                String right = result.substring(idx);
+                result = left +"(" +right+")";
+                return result;
+            }
+            
+            // save the reminder and index into map
+            map.put(reminder, result.length());
+            // get current quotient
+            res = reminder/den;
+            result += String.valueOf(res);
+            // get next reminder
+            reminder = (reminder%den) * 10;
+            
+        }
+        
+        return result;
+    }
+    
+    public static void main(String args[]) {
+        Fraction2Recurring f = new Fraction2Recurring();
+        System.out.println(f.fractionToDecimal(1, 6));
+        System.out.println(f.fractionToDecimal(-1, -2147483648));
     }
 }
