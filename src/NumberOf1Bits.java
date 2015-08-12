92d1280d7d49cff499c32dd1d08724a4584d9648
ghost2004 <liutg@hotmail.com>
2015-08-11 23:27:30 -0700

    Number of 1 Bits

---
+++ src/NumberOf1Bits.java
@@ -0,0 +1,23 @@
+/*
+ * Write a function that takes an unsigned integer and returns the number of ¡¯1' 
+ * bits it has (also known as the Hamming weight).
+
+For example, the 32-bit integer ¡¯11' has binary representation 
+00000000000000000000000000001011, so the function should return 3.
+ */
+public class NumberOf1Bits {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int cnt = 0;
+        int t = n;
+        while (t != 0) {
+            // remove the last one bit
+            t &= t -1;
+            cnt++;
+        }
+        
+        return cnt;
+    }
+    
+
+}
