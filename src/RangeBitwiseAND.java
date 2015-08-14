643efc6f24abefabef3e0b092085163d53c6e989
ghost2004 <liutg@hotmail.com>
2015-08-13 22:58:06 -0700

    Bitwise AND of Numbers Range

--- src/RangeBitwiseAND.java
+++ src/RangeBitwiseAND.java
@@ -5,7 +5,7 @@
 For example, given the range [5, 7], you should return 4.
  */
 public class RangeBitwiseAND {
-    public int rangeBitwiseAnd(int m, int n) {
+    public int rangeBitwiseAndTimeEx(int m, int n) {
         if (m == n)
             return m;
         if (m == 0)
@@ -15,5 +15,24 @@ public class RangeBitwiseAND {
             out = out & i;
         }
         return out;
+    }
+    
+    public int rangeBitwiseAnd2(int m, int n) {
+        while (n > m) {
+            n = n&(n-1);
+        }
+        
+        return m&n;
+    }
+    
+    public int rangeBitwiseAnd(int m, int n) {
+        int cnt = 0;
+        while (n != m) {
+            n  = n >> 1 ;
+            m = m >> 1;
+            cnt++;
+        }
+        
+        return m << cnt;
     }
 }
